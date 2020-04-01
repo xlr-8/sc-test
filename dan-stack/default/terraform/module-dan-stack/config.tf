3a5f0d708fbdded8963d422cc68c907c8fc498c2
Daniele Gazzelloni <daniele.gazzelloni@cycloid.io>
2020-04-01 13:44:21 +0000

    Update Service Catalog Terraform: dan-stack

---
+++ dan-stack/default/terraform/module-dan-stack/config.tf
@@ -0,0 +1,31 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "aws" {
+  "region" = "Forward"
+}
+
+"provider" "vault" {
+  "address" = "sensor"
+
+  "token" = "Rwanda"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}

135e6b5cc7d91baf35cfe86032988391cfedb989
Daniele Gazzelloni <daniele.gazzelloni@cycloid.io>
2020-04-13 13:07:19 +0000

    Update Service Catalog Terraform: my-stack

---
+++ my-stack/default/terraform/module-my-stack/config.tf
@@ -0,0 +1,31 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "aws" {
+  "region" = "violet"
+}
+
+"provider" "vault" {
+  "address" = "deposit"
+
+  "token" = "parse"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}

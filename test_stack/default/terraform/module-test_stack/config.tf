3459733c608afd89d4037974110f7772997d21fc
Daniele Gazzelloni <daniele.gazzelloni@cycloid.io>
2020-04-14 11:17:21 +0000

    Update Service Catalog Terraform: test_stack

---
+++ test_stack/default/terraform/module-test_stack/config.tf
@@ -0,0 +1,31 @@
+"locals" = {
+  "tags_cycloid" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "tags_stack" = {}
+}
+
+"provider" "aws" {
+  "region" = "Balboa US Dollar"
+}
+
+"provider" "vault" {
+  "address" = "eco-centric"
+
+  "token" = "Data"
+}
+
+"variable" "env" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}
+
+"variable" "project" {
+  "default" = "[PLACEHOLDER]"
+
+  "type" = "string"
+}

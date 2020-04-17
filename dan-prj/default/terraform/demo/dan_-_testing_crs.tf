fe5a15834ef47a8812d3dce8e05a29cf7bfa3c02
Daniele Gazzelloni <daniele.gazzelloni@cycloid.io>
2020-04-17 14:08:52 +0000

    Create Config Repository: Dan Repo (Default)

---
+++ dan-prj/default/terraform/demo/dan_-_testing_crs.tf
@@ -0,0 +1,9 @@
+module "tf-dan_-_testing_crs" {
+  source = "module-dan_-_testing_crs"
+
+  # env - type: string
+  # env = "[PLACEHOLDER]"
+
+  # project - type: string
+  # project = "[PLACEHOLDER]"
+}

031c61810d3ab87279c75610215782a91aa01885
Daniele Gazzelloni <daniele.gazzelloni@cycloid.io>
2020-04-28 09:29:21 +0000

    Create Config Repository: Dan CR

---
+++ dan-prj-2/default/terraform/dev/dan_scs_-_stacks_staging.tf
@@ -0,0 +1,9 @@
+module "tf-dan_scs_-_stacks_staging" {
+  source = "module-dan_scs_-_stacks_staging"
+
+  # env - type: string
+  # env = "[PLACEHOLDER]"
+
+  # project - type: string
+  # project = "[PLACEHOLDER]"
+}

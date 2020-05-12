52379b5869bfdcb22b8ba640b8117f0fb0fade80
Emily Carey <emily.carey@cycloid.io>
2020-05-12 08:07:19 +0000

    Create Config Repository: Dan CR

---
+++ testthings/terraform/dev/12-8-web-home-loan-account-201.tf
@@ -0,0 +1,9 @@
+module "tf-12-8-web-home-loan-account-201" {
+  source = "module-12-8-web-home-loan-account-201"
+
+  # env - type: string
+  # env = "[PLACEHOLDER]"
+
+  # project - type: string
+  # project = "[PLACEHOLDER]"
+}
---
+++ testthings/terraform/dev/8-web-home-loan-account-201.tf
@@ -0,0 +1,9 @@
+module "tf-8-web-home-loan-account-201" {
+  source = "module-8-web-home-loan-account-201"
+
+  # env - type: string
+  # env = "[PLACEHOLDER]"
+
+  # project - type: string
+  # project = "[PLACEHOLDER]"
+}

9e5862355560d8ebb52cd9cd6c752138c42e4bf9
Charline Moncoucut <charline.moncoucut@cycloid.io>
2020-10-26 10:28:53 +0000

    Create Config Repository by Dan CR

---
+++ testcha2/default/terraform/dev/nikola-stack-dep-test.tf
@@ -0,0 +1,9 @@
+module "tf-nikola-stack-dep-test" {
+  source = "module-nikola-stack-dep-test"
+
+  # env - type: string
+  # env = "[PLACEHOLDER]"
+
+  # project - type: string
+  # project = "[PLACEHOLDER]"
+}

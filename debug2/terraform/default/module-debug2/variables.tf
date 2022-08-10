349bf411f4722832effe463fb37d0c7d976a083d
given family <cycloidio@cycloid.io>
2022-08-10 14:28:59 +0000

    Update Service Catalog Terraform: debug2

---
+++ debug2/terraform/default/module-debug2/variables.tf
@@ -0,0 +1,28 @@
+variable "aws_instance_cycloid_bastion0_eu_we1_infra_ami" {
+  default = "ami-e079f893"
+}
+
+variable "aws_instance_cycloid_bastion0_eu_we1_infra_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_monitoring0_eu_we1_infra_ami" {
+  default = "ami-e079f893"
+}
+
+variable "aws_instance_cycloid_monitoring0_eu_we1_infra_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_monitoring_prometheus_eu_we1_infra_ami" {
+  default = "ami-0a889af68c555f069"
+}
+
+variable "aws_instance_monitoring_prometheus_eu_we1_infra_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_monitoring_prometheus_eu_we1_infra_ebs_optimized" {
+  default = true
+}
+

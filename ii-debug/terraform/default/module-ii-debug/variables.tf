de22791dbd917984114955fd508ceb4dcd402160
given family <cycloidio@cycloid.io>
2022-08-10 13:54:49 +0000

    Update Service Catalog Terraform: ii-debug

---
+++ ii-debug/terraform/default/module-ii-debug/variables.tf
@@ -0,0 +1,296 @@
+variable "aws_instance_azqxr_ami" {
+  default = "ami-0d4ae25eba73644c1"
+}
+
+variable "aws_instance_azqxr_availability_zone" {
+  default = "eu-west-1c"
+}
+
+variable "aws_instance_ccm_es_tests_cy_instances_0_elastic_cloud_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_ccm_es_tests_cy_instances_0_elastic_cloud_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_ccm_es_tests_cy_instances_0_es_t2_2xlarge_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_ccm_es_tests_cy_instances_0_es_t2_2xlarge_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_ccm_es_tests_cy_instances_0_es_t2_2xlarge_ebs_optimized" {
+  default = true
+}
+
+variable "aws_instance_ccm_es_tests_cy_instances_0_m6i_4xlarge_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_ccm_es_tests_cy_instances_0_m6i_4xlarge_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_ccm_es_tests_es_instances_es_t2_2xlarge_ami" {
+  default = "ami-02e52747ec1f04026"
+}
+
+variable "aws_instance_ccm_es_tests_es_instances_es_t2_2xlarge_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_ccm_es_tests_es_instances_m6i_4xlarge_ami" {
+  default = "ami-02e52747ec1f04026"
+}
+
+variable "aws_instance_ccm_es_tests_es_instances_m6i_4xlarge_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_bastion0_eu_we1_infra_ami" {
+  default = "ami-e079f893"
+}
+
+variable "aws_instance_cycloid_bastion0_eu_we1_infra_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_demo_workers_prod_ami" {
+  default = "ami-0028c9055f981f26e"
+}
+
+variable "aws_instance_cycloid_demo_workers_prod_availability_zone" {
+  default = "eu-west-1b"
+}
+
+variable "aws_instance_cycloid_fly_now_workers_demo_ami" {
+  default = "ami-0028c9055f981f26e"
+}
+
+variable "aws_instance_cycloid_fly_now_workers_demo_availability_zone" {
+  default = "eu-west-1b"
+}
+
+variable "aws_instance_cycloid_monitoring0_eu_we1_infra_ami" {
+  default = "ami-e079f893"
+}
+
+variable "aws_instance_cycloid_monitoring0_eu_we1_infra_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_devopscon_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_devopscon_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_fl_debian11_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_fl_debian11_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_gaelsmtp_ami" {
+  default = "ami-0ad0923fe3bd430fa"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_gaelsmtp_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_hr_generate_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_hr_generate_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_mg_inv_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_mg_inv_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_mg_inv_sp_1_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_mg_inv_sp_1_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_mg_org_tf_playground_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_mg_org_tf_playground_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_sg_complex_cond_ami" {
+  default = "ami-08894a8a0e419807d"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_sg_complex_cond_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_without_es_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_cycloid_onprem_cy_instances_0_without_es_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_cycloidio_website_front_prod_ami" {
+  default = "ami-0d4ae25eba73644c1"
+}
+
+variable "aws_instance_cycloidio_website_front_prod_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_dedicated_env_cy_instances_0_gl_cc_7_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_dedicated_env_cy_instances_0_gl_cc_7_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_dedicated_env_cy_instances_0_gl_ha_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_dedicated_env_cy_instances_0_gl_ha_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_dedicated_env_cy_instances_1_gl_ha_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_dedicated_env_cy_instances_1_gl_ha_availability_zone" {
+  default = "eu-west-1b"
+}
+
+variable "aws_instance_dedicated_env_cy_instances_2_gl_ha_ami" {
+  default = "ami-02fcaafc762651496"
+}
+
+variable "aws_instance_dedicated_env_cy_instances_2_gl_ha_availability_zone" {
+  default = "eu-west-1c"
+}
+
+variable "aws_instance_discoursereinventingorganizations_ami" {
+  default = "ami-5718412e"
+}
+
+variable "aws_instance_discoursereinventingorganizations_availability_zone" {
+  default = "eu-west-1b"
+}
+
+variable "aws_instance_dkeqz_ami" {
+  default = "ami-02e52747ec1f04026"
+}
+
+variable "aws_instance_dkeqz_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_glyqa_ami" {
+  default = "ami-0404d370443424a2e"
+}
+
+variable "aws_instance_glyqa_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_glyqa_ebs_optimized" {
+  default = true
+}
+
+variable "aws_instance_jjofl_ami" {
+  default = "ami-0404d370443424a2e"
+}
+
+variable "aws_instance_jjofl_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_jjofl_ebs_optimized" {
+  default = true
+}
+
+variable "aws_instance_magento_front0_eu_we1_demo_ami" {
+  default = "ami-402f1a33"
+}
+
+variable "aws_instance_magento_front0_eu_we1_demo_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_monitoring_prometheus_eu_we1_infra_ami" {
+  default = "ami-0a889af68c555f069"
+}
+
+variable "aws_instance_monitoring_prometheus_eu_we1_infra_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_monitoring_prometheus_eu_we1_infra_ebs_optimized" {
+  default = true
+}
+
+variable "aws_instance_onchh_ami" {
+  default = "ami-0404d370443424a2e"
+}
+
+variable "aws_instance_onchh_availability_zone" {
+  default = "eu-west-1b"
+}
+
+variable "aws_instance_onchh_ebs_optimized" {
+  default = true
+}
+
+variable "aws_instance_reinventingorganizationswiki_ami" {
+  default = "ami-402f1a33"
+}
+
+variable "aws_instance_reinventingorganizationswiki_availability_zone" {
+  default = "eu-west-1b"
+}
+
+variable "aws_instance_wjoyb_ami" {
+  default = "ami-02e52747ec1f04026"
+}
+
+variable "aws_instance_wjoyb_availability_zone" {
+  default = "eu-west-1a"
+}
+
+variable "aws_instance_workers_worker_eu_we1_prod_ami" {
+  default = "ami-0404d370443424a2e"
+}
+
+variable "aws_instance_workers_worker_eu_we1_prod_availability_zone" {
+  default = "eu-west-1c"
+}
+
+variable "aws_instance_workers_worker_eu_we1_prod_ebs_optimized" {
+  default = true
+}
+

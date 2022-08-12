3eae97f3830d03fdc7e52c6f041121c709ae1959
given family <cycloidio@cycloid.io>
2022-08-12 11:56:54 +0000

    Update Service Catalog Terraform: rs5

---
+++ rs5/terraform/default/module-rs5/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_instance_cycloid_bastion0_eu_we1_infra_ami" {
+  default = "ami-e079f893"
+}
+
+variable "aws_instance_cycloid_bastion0_eu_we1_infra_availability_zone" {
+  default = "eu-west-1a"
+}
+
